701a76803b136c8eecb97b04cc3a65b02a152c4e
e.alihangt@gmail.com <e.alihangt@gmail.com>
2019-11-28 21:30:18 +0600

    Add project files.

---
+++ GroupListElementsLesson/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GroupListElementsLesson
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public override string ToString()
+        {
+            return $"{Id} - {Name} - ${Price}";
+        }
+    }
+}
---
+++ GroupListElementsLesson/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GroupListElementsLesson
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<Item> items = new List<Item>
+            {
+                new Item
+                {
+                    Id = 1,
+                    Name = "HELB",
+                    Price = 150
+                },
+                new Item
+                {
+                    Id = 2,
+                    Name = "MYLO",
+                    Price = 150
+                },
+                new Item
+                {
+                    Id = 3,
+                    Name = "DOVE - ento ne mylo",
+                    Price = 150
+                }
+            };
+            //foreach (var item in items)
+            //{
+            //    listBox.Items.Add(item);
+            //}
+            listBox.ItemsSource = items;
+        }
+
+        private void ListBoxSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            MessageBox.Show($"Vybrannyi index - {listBox.SelectedIndex}");
+        }
+    }
+}
